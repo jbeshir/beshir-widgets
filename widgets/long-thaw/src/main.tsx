3d6c93adfc5e63ef108445841a6c4695bdcc6eb7
John Beshir <john@beshir.org>
2026-07-20 20:13:25 +0100

    Register Long Thaw with theme audit

--- widgets/long-thaw/src/main.tsx
+++ widgets/long-thaw/src/main.tsx
@@ -2,6 +2,7 @@ import { render } from 'preact';
 import '@fontsource/cinzel-decorative/700.css';
 import '@fontsource/nunito-sans/400.css';
 import '@fontsource/nunito-sans/700.css';
+import '@fontsource/schibsted-grotesk/700.css';
 import './theme.css';
 import { App } from './App';
 
